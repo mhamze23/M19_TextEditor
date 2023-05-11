fad1338dc08802e5b58225edec0a7bc702343125
mhamze23 <hamze.mohamed@live.ca>
2023-05-10 20:02:17 -0400

    minor fixes

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,12 +1,15 @@
+// Import required Workbox modules
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { StaleWhileRevalidate, CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+// Precache the assets specified in the Webpack-generated manifest
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Create a CacheFirst strategy for caching pages
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
@@ -14,30 +17,32 @@ const pageCache = new CacheFirst({
       statuses: [0, 200],
     }),
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
     }),
   ],
 });
 
+// Warm the pageCache with specified URLs
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
 
+// Register a route to cache pages using the CacheFirst strategy
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// Implement asset caching for images using Workbox
+// Register a route to cache images using the StaleWhileRevalidate strategy
 registerRoute(
   ({ request }) => request.destination === 'image',
-  new CacheFirst({
-    cacheName: 'my-image-cache',
+  new StaleWhileRevalidate({
+    cacheName: 'image-cache',
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
       new ExpirationPlugin({
         maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
       }),
     ],
   })
